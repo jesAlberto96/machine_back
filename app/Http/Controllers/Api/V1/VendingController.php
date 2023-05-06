d4604c01cc4fc1fec4d725a5952eb20fad00e5db
Jesus Alberto Garcia <jes.alberto96@gmail.com>
2023-05-05 19:15:41 -0500

    Ultimos ajustes en enpoints

--- app/Http/Controllers/Api/V1/VendingController.php
+++ app/Http/Controllers/Api/V1/VendingController.php
@@ -27,7 +27,15 @@ class VendingController extends Controller
 
         $this->updateCoins($request->coins);
 
-        return Response::sendResponse($product);
+        return Response::sendResponse(
+            array(
+                "product" => $product,
+                "change" => [
+                    "coin" => $this->data_change["coin"],
+                    "total_coins" => $this->data_change["total_coins"],
+                ]
+            )
+        );
     }
 
     private function validateBuy($request, $product){
@@ -43,7 +51,6 @@ class VendingController extends Controller
             return false;
         } else {
             $change = number_format(($total_coins - $product->price), 2);
-
             $coins = CoinRepository::getAllByWhere(array(
                 ['amount', '>', 0],
                 ['coin', '<=', $change]
@@ -77,22 +84,29 @@ class VendingController extends Controller
             ]);
         }
 
-        CoinRepository::update($this->data_change["coin"], [
-            "amount" => ($this->data_change["coin"]->amount - $this->data_change["total_coins"])
-        ]);
+        if ($this->data_change["total_coins"] != 0){
+            CoinRepository::update($this->data_change["coin"], [
+                "amount" => ($this->data_change["coin"]->amount - $this->data_change["total_coins"])
+            ]);
+        }
 
         return true;
     }
 
     private function getChange($coins, $change){
-        foreach ($coins as $key => $coin) {
-            $coin_change = number_format(($change / $coin->coin), 2);
-            if (!(fmod($coin_change, 1) !== 0.00)){
-                if ((int) $coin->amount >= intval($coin_change)){
-                    return array("coin" => $coin, "total_coins" => intval($coin_change));
+        if ($change == 0.00){ //Don´t need change
+            return array("coin" => 0, "total_coins" => 0);
+        } else {
+            foreach ($coins as $key => $coin) {
+                $coin_change = number_format(($change / $coin->coin), 2);
+                if (!(fmod($coin_change, 1) !== 0.00)){
+                    if ((int) $coin->amount >= intval($coin_change)){
+                        return array("coin" => $coin, "total_coins" => intval($coin_change));
+                    }
                 }
             }
         }
+
         return false;
     }
 }
